9436401c1c5a05d13dc2bf4454d5b50877f203c9
Maxifloresta <maximilianoemariani@gmail.com>
2024-07-26 17:54:11 -0300

    Create Modulo Dados.py

---
+++ Modulo Dados.py
@@ -0,0 +1,16 @@
+# Módulo dados.py
+
+def lanzar_dados(num_dados=5):
+    return [random.randint(1, 6) for _ in range(num_dados)]
+
+def mostrar_dados(dados):
+    print("Resultados del lanzamiento: ", dados)
+
+def obtener_frecuencias(dados):
+    frecuencias = {}
+    for dado in dados:
+        if dado in frecuencias:
+            frecuencias[dado] += 1
+        else:
+            frecuencias[dado] = 1
+    return frecuencias
